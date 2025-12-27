dbbe73e1e9d824c6e7c143c26816ec1417be533b
jardeloli <jardelsantosa380@gmail.com>
2025-12-27 05:37:35 -0300

    adicionado validação de entrada

--- api/gestao_epi/Gestão_Epi/Models/Login_Model/LoginRequest.cs
+++ api/gestao_epi/Gestão_Epi/Models/Login_Model/LoginRequest.cs
@@ -1,10 +1,14 @@
-﻿namespace Gestão_Epi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gestão_Epi.Models
 {
     public class LoginRequest
     {
-        public string? Email { get; set; }
-        public required string Senha { get; set; }
-        
+        [Required(ErrorMessage = "O campo Email é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O campo Email deve ser um endereço de email válido.")]
+        public string? Email { get; set; } = string.Empty;
+        public required string Senha { get; set; } = string.Empty;
+
 
     }
 }
