99c1b68ce56c3d6d5d420dfb706741463b28f911
jardeloli <jardelsantosa380@gmail.com>
2025-12-27 10:14:22 -0300

    criado controller colaborador

---
+++ api/gestao_epi/Gestão_Epi/Controllers/Colaborador_Controller.cs
@@ -0,0 +1,20 @@
+﻿using Gestão_Epi.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gestão_Epi.Controllers
+{
+    [Route("api/colaborador")]
+    [ApiController]
+    public class Colaborador_Controller : ControllerBase
+    {
+        private readonly AppDbContext _bancoGE;
+
+        public Colaborador_Controller(AppDbContext bancoGE)
+        {
+            _bancoGE = bancoGE;
+        }
+
+        
+    }
+}
