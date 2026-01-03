9ee8dedcd528b6be31ade2e66ab19849095a55e9
jardeloli <jardelsantosa380@gmail.com>
2026-01-03 16:12:34 -0300

    controller visitante criado e adicionado o endpoint listar visitantes. visitantes poderam retirar epi, mas deverão devolver ao final da sua passagem

---
+++ api/gestao_epi/Gestão_Epi/Controllers/Visitante_Controller.cs
@@ -0,0 +1,32 @@
+﻿using Gestão_Epi.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gestão_Epi.Controllers
+{
+    [Route("api/visitante")]
+    [ApiController]
+    public class Visitante_Controller : ControllerBase
+    {
+        private readonly AppDbContext _bancoGE;
+
+        public Visitante_Controller(AppDbContext bancoGE)
+        {
+            _bancoGE = bancoGE;
+        }
+
+        [HttpGet("listar-visitantes")]
+        public async Task<IActionResult> Listar_Visitsantes()
+        {
+            var visitante = await _bancoGE.visitante.ToListAsync();
+
+            if (visitante == null || visitante.Count == 0)
+            {
+                return NotFound ("Nenhum visitante encontrado.");
+            }
+
+            return Ok(visitante);
+        }
+    }
+}
