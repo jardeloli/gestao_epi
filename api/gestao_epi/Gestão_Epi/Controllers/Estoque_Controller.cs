b7e68b3c7dcfc99dd7ed7baa3959111b63fbae88
jardeloli <jardelsantosa380@gmail.com>
2026-01-12 21:20:31 -0300

    controller estoque criado e adicionado o endpoint listar estoque

---
+++ api/gestao_epi/Gestão_Epi/Controllers/Estoque_Controller.cs
@@ -0,0 +1,27 @@
+﻿using Gestão_Epi.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gestão_Epi.Controllers
+{
+    [Route("api/estoque")]
+    [ApiController]
+    public class Estoque_Controller : ControllerBase
+    {
+        private readonly AppDbContext _bancoGE;
+
+        public Estoque_Controller(AppDbContext bancoGE)
+        {
+            _bancoGE = bancoGE;
+        }
+
+        [HttpGet("listar-estoque")]
+        public async Task<IActionResult> Listar_Estoque()
+        {
+            var estoque = await _bancoGE.estoque.ToListAsync();
+
+            return Ok(estoque);
+        }
+    }
+}
