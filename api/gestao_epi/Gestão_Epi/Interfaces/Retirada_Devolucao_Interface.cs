16f1c2ec9b90bbe90ff4d67d558724b906a60f9e
jardeloli <jardelsantosa380@gmail.com>
2026-01-14 00:00:43 -0300

    aplica interface no service de retirada_devolucao

---
+++ api/gestao_epi/Gestão_Epi/Interfaces/Retirada_Devolucao_Interface.cs
@@ -0,0 +1,24 @@
+﻿namespace Gestão_Epi.Interfaces
+{
+    public interface IRetiradaDevolucaoService
+    {
+        Task RegistrarRetiradaAsync(
+            int usuario_id,
+            int? colaborador_id,
+            int? visitante_id,
+            int epi_id,
+            int quantidade,
+            string? justificativa_retirada
+            );
+
+        Task RegistrarDevolucaoAsync(
+            int usuario_id,
+            int retirada_devolucao_id,
+            string? justificativa_devolucao
+            );
+
+        Task<IEnumerable<object>> ListarAsync();
+
+        Task<object> BuscarPorIdAsync(int id);
+    }
+}
