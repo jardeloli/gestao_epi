395900ab97c13cbc2e55f707e8abf3d34703e6b6
jardeloli <jardelsantosa380@gmail.com>
2026-01-13 23:56:59 -0300

    Adicionado atributo quantidade na classe retirada_devolucao

---
+++ api/gestao_epi/Gestão_Epi/Controllers/Retirada_Devolucao_Controller.cs
@@ -0,0 +1,37 @@
+﻿using Gestão_Epi.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gestão_Epi.Controllers
+{
+    [Route("api/retirada-devolucao")]
+    [ApiController]
+    public class Retirada_Devolucao_Controller : ControllerBase
+    {
+        private readonly IRetiradaDevolucaoService _retiradaDevolucaoService;
+
+        public Retirada_Devolucao_Controller(IRetiradaDevolucaoService retiradaDevolucaoService)
+        {
+            _retiradaDevolucaoService = retiradaDevolucaoService;
+        }
+
+        [HttpPost("registrar-retirada")]
+        public async Task<IActionResult> RegistrarRetirada(
+            int usuario_id,
+            int? colaborador_id,
+            int? visitante_id,
+            int epi_id,
+            int quantidade,
+            string? justificativa_retirada)
+        {
+            await _retiradaDevolucaoService.RegistrarRetiradaAsync(
+                usuario_id,
+                colaborador_id,
+                visitante_id,
+                epi_id,
+                quantidade,
+                justificativa_retirada);
+            return Ok("Retirada registrada com sucesso");
+        }
+    }
+}
