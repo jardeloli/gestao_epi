c13a106d680d76ad19f7d8f96c76e978295c3a04
jardeloli <jardelsantosa380@gmail.com>
2026-01-17 19:28:46 -0300

    aplica interface no service de notificacao

---
+++ api/gestao_epi/Gestão_Epi/Interfaces/Notificacao_Interface.cs
@@ -0,0 +1,15 @@
+﻿using Gestão_Epi.Entities;
+
+namespace Gestão_Epi.Interfaces
+{
+    public interface INotificacaoService
+    {
+        Task RegistrarNotificacaoAsync(
+            int visitante_id,
+            int retirada_id,
+            DateTime dataLimite,
+            string mensagem
+            );
+       
+    }
+}
