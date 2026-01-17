cfcbfd18242ad130060e6ac34c1bdfe6f9054bc6
jardeloli <jardelsantosa380@gmail.com>
2026-01-17 19:24:39 -0300

    criar notificacao automatica para retirada de visitante

--- api/gestao_epi/Gestão_Epi/Services/NotificacaoService.cs
+++ api/gestao_epi/Gestão_Epi/Services/NotificacaoService.cs
@@ -1,6 +1,37 @@
-﻿namespace Gestão_Epi.Services
+﻿using Gestão_Epi.Data;
+using Gestão_Epi.Entities;
+using Gestão_Epi.Interfaces;
+
+namespace Gestão_Epi.Services
 {
-    public class NotificacaoService
+    public class NotificacaoService: INotificacaoService
     {
+        private readonly AppDbContext _bancoGE;
+
+        public NotificacaoService(AppDbContext bancoGE)
+        {
+            _bancoGE = bancoGE;
+        }
+
+        public async Task RegistrarNotificacaoAsync(
+        int visitante_id,
+        int retirada_id,
+        DateTime dataLimite,
+        string mensagem)
+        {
+            var notificacao = new Notificacao
+            {
+                mensagem = mensagem,
+                visitante_id = visitante_id,
+                retirada_id = retirada_id,
+                data_limite = dataLimite,
+                status = "Pendente",
+                data_devolucao = null,
+                colaborador_id = null
+            };
+
+            _bancoGE.notificacao.Add(notificacao);
+            await  _bancoGE.SaveChangesAsync();
+        }
     }
 }
