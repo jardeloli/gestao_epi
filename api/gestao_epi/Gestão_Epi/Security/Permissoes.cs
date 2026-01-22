413ba05764dc82e2eac5a70aeb21a77adcf6a28e
jardeloli <jardelsantosa380@gmail.com>
2026-01-21 22:54:51 -0300

    feat: cria classe de permissões fixas para autorização

---
+++ api/gestao_epi/Gestão_Epi/Security/Permissoes.cs
@@ -0,0 +1,47 @@
+﻿namespace Gestão_Epi.Security
+{
+    public static class Permissoes
+    {
+        // EPI
+        public const string EPI_CADASTRAR = "EPI_CADASTRAR";
+        public const string EPI_EDITAR = "EPI_EDITAR";
+        public const string EPI_DELETAR = "EPI_DELETAR";
+        public const string EPI_LISTAR = "EPI_LISTAR";
+
+        // USUÁRIO
+        public const string USUARIO_CADASTRAR = "USUARIO_CADASTRAR";
+        public const string USUARIO_EDITAR = "USUARIO_EDITAR";
+        public const string USUARIO_DELETAR = "USUARIO_DELETAR";
+        public const string USUARIO_LISTAR = "USUARIO_LISTAR";
+
+        // PERFIL
+        public const string PERFIL_CADASTRAR = "PERFIL_CADASTRAR";
+        public const string PERFIL_EDITAR = "PERFIL_EDITAR";
+        public const string PERFIL_DELETAR = "PERFIL_DELETAR";
+        public const string PERFIL_LISTAR = "PERFIL_LISTAR";
+
+        // RETIRADA/DEVOLUÇÃO
+        public const string RETIRADA_REGISTRAR = "RETIRADA_REGISTRAR";
+        public const string DEVOLUCAO_REGISTRAR = "DEVOLUCAO_REGISTRAR";
+
+        // ESTOQUE
+        public const string ESTOQUE_LISTAR = "ESTOQUE_LISTAR";
+        public const string ESTOQUE_ENTRADA = "ESTOQUE_ENTRADA";
+        public const string ESTOQUE_SAIDA = "ESTOQUE_SAIDA";
+
+        // VISITANTE
+        public const string VISITANTE_CADASTRAR = "VISITANTE_CADASTRAR";
+        public const string VISITANTE_EDITAR = "VISITANTE_EDITAR";
+        public const string VISITANTE_DELETAR = "VISITANTE_DELETAR";
+        public const string VISITANTE_LISTAR = "VISITANTE_LISTAR";
+
+        // COLABORADOR
+        public const string COLABORADOR_CADASTRAR = "COLABORADOR_CADASTRAR";
+        public const string COLABORADOR_EDITAR = "COLABORADOR_EDITAR";
+        public const string COLABORADOR_DELETAR = "COLABORADOR_DELETAR";
+        public const string COLABORADOR_LISTAR = "COLABORADOR_LISTAR";
+
+
+
+    }
+}
