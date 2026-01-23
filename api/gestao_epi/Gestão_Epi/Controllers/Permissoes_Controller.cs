cbb128a895752b0043cd6628a7b42db44e825e77
jardeloli <jardelsantosa380@gmail.com>
2026-01-22 21:44:01 -0300

    feat: adiciona controller permissões, com apenas o endpoit de listagem

---
+++ api/gestao_epi/Gestão_Epi/Controllers/Permissoes_Controller.cs
@@ -0,0 +1,32 @@
+﻿using Gestão_Epi.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gestão_Epi.Controllers
+{
+    [Route("api/permissoes")]
+    [ApiController]
+    public class Permissoes_Controller : ControllerBase
+    {
+        private readonly AppDbContext _bancoGE;
+
+        public Permissoes_Controller(AppDbContext bancoGE)
+        {
+            _bancoGE = bancoGE;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Listar_Permissoes()
+        {
+            var permissoes = await _bancoGE.permissao.ToListAsync();
+
+            if (permissoes == null || permissoes.Count == 0)
+            {
+                return NotFound("Nenhuma permissão cadastrada.");
+            }
+
+            return Ok(permissoes);
+        }
+    }
+}
