f4c850327224241b3668e5586b635e4c8d6003d6
jardeloli <jardelsantosa380@gmail.com>
2026-01-22 22:39:46 -0300

    feat: cria entidade Perfil_Permissao e mapeamento N:N de permissões

---
+++ api/gestao_epi/Gestão_Epi/Entities/perfil_permissao.cs
@@ -0,0 +1,11 @@
+﻿namespace Gestão_Epi.Entities
+{
+    public class Perfil_Permissao
+    {
+        public int perfil_id { get; set; }
+        public Perfil perfil { get; set; } = null!;
+
+        public int permissao_id { get; set; }
+        public Permissao permissao { get; set; } = null!;
+    }
+}
