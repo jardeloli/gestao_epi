c5fb71eb987dd630c72f43a40348547d7a19f521
jardeloli <jardelsantosa380@gmail.com>
2026-01-24 16:03:15 -0300

    feat: adiciona Model PerfilResponse

--- api/gestao_epi/Gestão_Epi/Models/Perfil_Model/PerfilResponse.cs
+++ api/gestao_epi/Gestão_Epi/Models/Perfil_Model/PerfilResponse.cs
@@ -2,5 +2,11 @@
 {
     public class PerfilResponse
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public List<string> Permissoes { get; set; }
     }
+
 }
+
